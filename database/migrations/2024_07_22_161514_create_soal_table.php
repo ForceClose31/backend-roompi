536d95d2433701f9687e1620b5fd63178a2bbf00
ForceClose31 <forceclose31@gmail.com>
2024-07-25 15:06:11 +0700

    add seeder soal paket C

--- database/migrations/2024_07_22_161514_create_soal_table.php
+++ database/migrations/2024_07_22_161514_create_soal_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('soal', function (Blueprint $table) {
             $table->id();
-            $table->string('pertanyaan');
+            $table->text('pertanyaan');
             $table->unsignedBigInteger('sub_bagian_id');
             $table->unsignedBigInteger('paket_id');
             $table->unsignedBigInteger('category_id');
